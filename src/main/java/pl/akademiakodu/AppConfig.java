1a3540b86e53f3abe75dd5e13b0d5975ccf6742d
Damian Mielcarekan <damian.mielcarek@gmail.com>
2016-12-08 13:56:49 +0100

    initial commit

---
+++ src/main/java/pl/akademiakodu/AppConfig.java
@@ -0,0 +1,15 @@
+package pl.akademiakodu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Lenovo on 2016-12-08.
+ */
+
+@SpringBootApplication
+public class AppConfig {
+    public static void main(String[] args) {
+        SpringApplication.run(AppConfig.class,args);
+    }
+}
